24827d62a21e4ef49bd94b5f51ed6b78b7ecce70
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-02-25 15:48:39 +0200

    File moved to recursion Folder and other chalenges added

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+import Recursive.*;
+
+public class Main {
+    public static void main(String[] args) {        
+        Recursive recusive = new Recursive();
+    //    System.out.println(recusive.factorial(41));
+    //    System.out.println(recusive.fibonacci(42));
+     //  System.out.println(recusive.intToBinary(1563749802));
+        recusive.recursivePrintNumbers(344);
+    //    System.out.println(recusive.sumOfdigit(42345333));
+    //    System.out.println(recusive.gratherComunDivisor(4,45));
+    //    System.out.println(recusive.power(5,11));
+        
+    }
+}
---
+++ Recursive/Recursive.java
@@ -0,0 +1,30 @@
+package Recursive;
+
+public  class Recursive{
+    public  int recursivePrintNumbers(int number){
+        System.out.println(number);
+        if ( number < 1 ) return number;  return recursivePrintNumbers(number -1) + number;
+     }
+     public  int  factorial(int number) {
+         if(number <= 1) return 1; return factorial(number-1) * number;
+     }
+     public  int fibonacci( int number) {
+         if(number <= 0 ) return 0; 
+         if(number == 1 ) return 1;return (fibonacci(number-1 ) + fibonacci(number - 2));
+     }
+     public  int sumOfdigit(int number) {
+      if(number <= 0 ) return number;
+     return (number%10) +  sumOfdigit((number - (number%10))/10);
+    }
+    public  int  power(int number,int power) {
+      return  (power < 1) ? 1 : number * power(number, --power);
+    }
+    public  int gratherComunDivisor(int aNumber,int bNumber) {
+      if(aNumber < 0 || bNumber < 0 ) return -1;
+      if(bNumber == 0) return aNumber; return gratherComunDivisor(bNumber,aNumber%bNumber);
+    }
+    public String intToBinary(int number) {
+      if(number == 0 ) return "";
+      return   (intToBinary(number/2) ) +""+ (number%2);
+    }
+}
