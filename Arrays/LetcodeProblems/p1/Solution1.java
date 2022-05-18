b386a9d18e92843a18d3aa301a3bf64b82e5f725
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-05-18 12:53:45 +0200

    Unizambeze solutions not organized yet

--- Arrays/LetcodeProblems/p1/Solution1.java
+++ Arrays/LetcodeProblems/p1/Solution1.java
@@ -1,7 +1,18 @@
 package Arrays.LetcodeProblems.p1;
 
 public class Solution1 {
-    // public  int[] sumTwo(int[] array,int target) {
-        
-    // }
+    public  int[] sumTwo(int[] nums,int target) {
+        int indeces[] = {-1,-1};
+        if (nums.length < 2) {System.err.println("Invalid nums"); return indeces;}
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    indeces[0] = j;
+                    indeces[1] = i;
+                    break;
+                }
+            }
+        }
+        return indeces;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 
 
-import LikedLists.*;
+import Arrays.LetcodeProblems.p3.Solution3;
+
+// import LikedLists.*;
 
 /**
  * @author Nelson Alexandre Mutane
@@ -10,24 +12,59 @@ import LikedLists.*;
 public class Main {
     public static void main(String[] args) {
 
-        DoubleLinkedList lists = new DoubleLinkedList();
-        lists.insertEnd(15);
-        lists.insertEnd(154);
-        lists.insertEnd(155);
-        lists.insertAt(3,1545);
-        System.out.println(lists.toString());
-        System.out.println(lists.getLenght());
-        lists.deleteAt(0);
-        lists.deleteAt(0);
-        lists.deleteAt(0);
-        lists.deleteAt(0);
-        lists.insertEnd(155);
-        lists.insertEnd(155);
-        lists.insertEnd(155);
-        lists.insertEnd(155);
+        // DoubleLinkedList lists = new DoubleLinkedList();
+        // lists.insertEnd(15);
+        // lists.insertEnd(154);
+        // lists.insertEnd(155);
+        // lists.insertAt(3,1545);
+        // System.out.println(lists.toString());
+        // System.out.println(lists.getLenght());
+        // lists.deleteAt(0);
+        // lists.deleteAt(0);
+        // lists.deleteAt(0);
+        // lists.deleteAt(0);
+        // lists.insertEnd(155);
+        // lists.insertEnd(155);
+        // lists.insertEnd(155);
+        // lists.insertEnd(155);
 
-        lists.updateAt(4, 4);
-        System.out.println(lists.getLenght());
-        System.out.println(lists.toString());
+        // lists.updateAt(4, 4);
+        // System.out.println(lists.getLenght());
+        // System.out.println(lists.toString());
+
+        // Solution3 s3 = new Solution3();
+
+         int ar[] = {0,2,1,5,3,4};
+
+
+
+            
+         
+         
+
+        //  int ans[] = new int[ar.length];
+
+        //  for (int i = 0; i < ar.length; i++) {
+        //      ans[i] = ar[ar[i]];
+        //  }
+
+        //  for (int i : ans) {
+        //      System.out.println(ans[i]);
+        //  }
+ 
+        // // System.out.println(s3.removeElement(ar, 3));
+        // 1,2 ,3 ,4,5 ,6,7, 8,9,10
+            sum(1,2,3,4);
+    }
+
+
+    public static void sum(int ...nums) {
+        int sum =0;
+        int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length; i++){
+            sum += nums[i];
+            ans[i] = sum;
+            System.out.print("," + ans[i]);
+        }
     }
 }
